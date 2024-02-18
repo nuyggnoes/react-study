e3020fa58a57b753a76ca838b1fcd8281bc8987f
임성균 <97024187+nuyggnoes@users.noreply.github.com>
2024-02-18 17:44:00 +0900

    react router dom (5.3.4 버전 사용)

--- src/App.js
+++ src/App.js
@@ -1,30 +1,18 @@
-import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
   return (
-    <div>
-      <h1>The Coins! ({coins.length})</h1>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <select>
-          {coins.map((coin) => (
-            <option key={coin.id}>
-              {coin.name} ({coin.symbol}): {coin.quotes.USD.price}
-            </option>
-          ))}
-        </select>
-      )}
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/movie">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
